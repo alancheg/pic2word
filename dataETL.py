43e144d2d0f55c88e918fb4e641ab72f16038f43
alancheg <chenggui53@gmail.com>
2017-02-28 20:22:57 +0800

    v0.1

--- dataETL.py
+++ dataETL.py
@@ -17,9 +17,44 @@ def data_etl():
     # 文件根目录
     root_path = 'D:\\# nlp\\flikr_8k_min\\'
     pic_data = 'pic\\'
+    token = 'Flickr8k.token.txt'
 
-    pic_list = os.path.dirname(root_path)
-    print(pic_list)
+    # 训练数据，key 是图片的文件名称，value 是它的一句话描述
+    train_dict = {}
+
+    # 列出所有的图片文件
+    filename_list = []
+    for parent, dirnames, filenames in os.walk(root_path):
+        for filename in filenames:
+            # print(filename)
+            filename_list.append(filename)
+
+    # 找出它们对应的语句
+    if os.path.exists(root_path + token):
+        token_list = list(open(root_path + token))
+
+    # 遍历，将图片和对应的描述一一对应
+    for token in token_list:
+        for filename in filename_list:
+            if filename in token:
+
+                # print('---- start ----')
+                # print(filename)
+                # print(token)
+                # print('---- end ----')
+
+                token_list.remove(token)
+                filename_list.remove(filename)
+
+                train_dict.setdefault(filename,' ')
+
+                # token.replace(filename, ' ')
+                # token.replace('#', ' ')
+                train_dict[filename] = token.replace(filename, ' ').replace('#', ' ')[4:]
+
+    for key in train_dict.keys():
+        print(key)
+        print(train_dict[key])
 
 if __name__ == "__main__":
     data_etl()
